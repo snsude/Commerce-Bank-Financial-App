f4cdc24899ba59bc70f1c3abe3629b1451a372be
A-Mohammed-19 <armwp7@umsystem.edu>
2025-10-07 10:37:49 -0500

    React Env

---
+++ ClariFi/frontend/src/App.jsx
@@ -0,0 +1,10 @@
+import './App.css'
+import './index.css'
+
+function App() {
+  return <h1 className="text-4xl font-bold text-blue-600">Clarifi + Tailwind 🚀</h1>;
+}
+export default App;
+
+
+
---
+++ ClariFi/frontend/src/pages/Login.jsx
@@ -0,0 +1,7 @@
+import React, {useState} from 'react';
+import {useNavigate} from 'react-router-dom';
+
+const Login = () =>{
+    const [email,setEmail] = useState('');
+    const [password,setPassword] = useState('');
+}
