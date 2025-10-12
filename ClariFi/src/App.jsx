80dc5bc58d88698d88c564ff8f312e3b48209c1c
Haley Nilsen <hmnhpd@umsystem.edu>
2025-10-12 10:50:15 -0500

    Frontend Progress

--- ClariFi/src/App.jsx
+++ ClariFi/src/App.jsx
@@ -1,9 +1,15 @@
-import './App.css'
 import './index.css'
+import { BrowserRouter } from 'react-router-dom'
+import Login from './pages/Login'
 
 function App() {
-  return <h1 className="text-4xl font-bold text-blue-600">Clarifi + Tailwind 🚀</h1>;
+  return (
+    <BrowserRouter>
+      <Login />
+    </BrowserRouter>
+  );
 }
+
 export default App;
 
 
